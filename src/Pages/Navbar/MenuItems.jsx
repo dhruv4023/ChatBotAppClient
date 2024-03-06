a7c832a4f14bfd01001087a3ba8bd96701b89309
dhruv <dhruv20345@gmail.com>
2024-03-07 01:32:32 +0530

    Navbar updtaed V1

--- src/Pages/Navbar/MenuItems.jsx
+++ src/Pages/Navbar/MenuItems.jsx
@@ -1,10 +1,10 @@
-import { DarkMode, LightMode, LoginRounded, Logout } from '@mui/icons-material'
-import { setMode, setLogout } from '../../state/index'
-import { IconButton, Tooltip } from '@mui/material'
 import React from 'react'
+import { DarkMode, LightMode, LoginRounded, Logout } from '@mui/icons-material'
+import { IconButton, Tooltip } from '@mui/material'
 import { useDispatch, useSelector } from 'react-redux'
 import { useTheme } from '@emotion/react'
 import { useNavigate } from 'react-router-dom'
+import { setMode, setLogout } from '../../state/index'
 import UserImg from '../../Components/UserImg'
 
 const MenuItems = () => {
@@ -12,56 +12,49 @@ const MenuItems = () => {
   const theme = useTheme()
   const navigate = useNavigate()
   const dark = theme.palette.neutral.dark
-  const user = useSelector(s => s.user)
+  const user = useSelector(state => state.user)
+
+  const toggleMode = () => {
+    dispatch(setMode())
+  }
+
+  const handleLogout = () => {
+    dispatch(setLogout())
+    navigate('/')
+  }
 
   return (
     <>
-      <IconButton gap={'1rem'} onClick={() => dispatch(setMode())}>
-        {theme.palette.mode === 'dark' ? (
-          <Tooltip title='light mode'>
+      <IconButton onClick={toggleMode}>
+        <Tooltip
+          title={theme.palette.mode === 'dark' ? 'Light mode' : 'Dark mode'}
+        >
+          {theme.palette.mode === 'dark' ? (
             <LightMode sx={{ fontSize: '25px' }} />
-          </Tooltip>
-        ) : (
-          <Tooltip title='dark mode'>
+          ) : (
             <DarkMode sx={{ fontSize: '25px', color: dark }} />
-          </Tooltip>
-        )}
-      </IconButton>
-      {/* <IconButton onClick={() => navigate('/contact')}>
-        <Tooltip title='Contact Support'>
-          <ContactSupport sx={{ fontSize: '25px' }} />
+          )}
         </Tooltip>
-      </IconButton> */}
+      </IconButton>
       {user ? (
         <>
-          <Tooltip title={`${user?.firstName} ${user?.lastName}`}>
-            <IconButton onClick={() => navigate(`/profile/${user?.username}`)}>
-              <UserImg image={user?.picPath} size={'30px'} />
+          <Tooltip title={`${user.firstName} ${user.lastName}`}>
+            <IconButton onClick={() => navigate(`/profile/${user.username}`)}>
+              <UserImg image={user.picPath} size={'30px'} />
             </IconButton>
           </Tooltip>
-          <IconButton
-            onClick={() => {
-              dispatch(setLogout())
-              navigate('/')
-            }}
-          >
+          <IconButton onClick={handleLogout}>
             <Tooltip title='Log out'>
               <Logout />
             </Tooltip>
           </IconButton>
         </>
       ) : (
-        <>
-          <IconButton
-            onClick={() => {
-              navigate('/auth/login')
-            }}
-          >
-            <Tooltip title='Login'>
-              <LoginRounded sx={{ cursor: 'pointer' }} />
-            </Tooltip>
-          </IconButton>
-        </>
+        <IconButton onClick={() => navigate('/auth/login')}>
+          <Tooltip title='Login'>
+            <LoginRounded sx={{ cursor: 'pointer' }} />
+          </Tooltip>
+        </IconButton>
       )}
     </>
   )
--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,49 +1,45 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import {
   Box,
   IconButton,
   Typography,
   useTheme,
-  useMediaQuery,
-} from "@mui/material";
-import {  Menu, Close } from "@mui/icons-material";
-import { useNavigate } from "react-router-dom";
-import FlexBetween from "../../Components/FlexBetween";
-import MenuItems from "./MenuItems";
+  useMediaQuery
+} from '@mui/material'
+import { Menu, Close } from '@mui/icons-material'
+import { useNavigate } from 'react-router-dom'
+import FlexBetween from '../../Components/FlexBetween'
+import MenuItems from './MenuItems'
 
 export const Navbar = () => {
-  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
-  const isNonMobileScreens = useMediaQuery("(min-width: 600px)");
-
-  // const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const theme = useTheme();
-  // const neutralLight = theme.palette.neutral.light;
-  // const dark = theme.palette.neutral.dark;
-  const background = theme.palette.background.default;
-  const primaryLight = theme.palette.primary.light;
-  const alt = theme.palette.background.alt;
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false)
+  const isNonMobileScreens = useMediaQuery('(min-width: 600px)')
+  const navigate = useNavigate()
+  const theme = useTheme()
+  const background = theme.palette.background.default
+  const primaryLight = theme.palette.primary.light
+  const alt = theme.palette.background.alt
 
   return (
     <>
-      <FlexBetween backgroundColor={alt} padding="1rem 6%">
+      <FlexBetween backgroundColor={alt} padding='1rem 6%'>
         <Typography
-          color={"primary"}
-          onClick={() => navigate("/")}
-          fontWeight={"bold"}
-          fontSize={"clamp(1rem,1.5rem,2rem)"}
+          color={'primary'}
+          onClick={() => navigate('/')}
+          fontWeight={'bold'}
+          fontSize={'clamp(1rem,1.5rem,2rem)'}
           sx={{
-            "&:hover": {
+            '&:hover': {
               color: primaryLight,
-              cursor: "pointer",
-            },
+              cursor: 'pointer'
+            }
           }}
         >
           LLM Chat App
         </Typography>
         {isNonMobileScreens ? (
           <>
-            <FlexBetween gap={"1rem"}>
+            <FlexBetween gap={'1rem'}>
               <MenuItems />
             </FlexBetween>
           </>
@@ -57,49 +53,44 @@ export const Navbar = () => {
         {!isNonMobileScreens && isMobileMenuToggled && (
           <>
             <Box
-              position={"fixed"}
-              right="0"
-              top="0"
-              zIndex="10"
-              maxWidth="500px"
-              minWidth="30px"
+              position={'fixed'}
+              right='0'
+              top='0'
+              zIndex='10'
+              maxWidth='500px'
+              minWidth='30px'
               backgroundColor={background}
-              // border="1px solid red"
-              height={"50%"}
-              overflow={"auto"}
+              height={'50%'}
+              overflow={'auto'}
             >
               <FlexBetween
-                display="flex"
-                flexDirection="column"
-                justifyContent="center"
-                alignItems="center"
-                gap="3rem"
+                display='flex'
+                flexDirection='column'
+                justifyContent='center'
+                alignItems='center'
+                gap='3rem'
               >
                 <Box
-                  display={"flex"}
-                  zIndex={"11"}
-                  justifyContent={"flex-end"}
-                  p={"1rem"}
+                  display={'flex'}
+                  zIndex={'11'}
+                  justifyContent={'flex-end'}
+                  p={'1rem'}
                 >
-                  <IconButton
-                    onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
-                  >
+                  <IconButton onClick={() => setIsMobileMenuToggled(false)}>
                     <Close />
                   </IconButton>
                 </Box>
                 <MenuItems />
               </FlexBetween>
-              <IconButton
-                onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
-              >
+              <IconButton onClick={() => setIsMobileMenuToggled(false)}>
                 <Box
                   sx={{
-                    position: "fixed",
+                    position: 'fixed',
                     left: 0,
                     zIndex: -1,
                     bottom: 0,
-                    width: "100%",
-                    height: "100%",
+                    width: '100%',
+                    height: '100%'
                   }}
                 />
               </IconButton>
@@ -108,5 +99,5 @@ export const Navbar = () => {
         )}
       </FlexBetween>
     </>
-  );
-};
+  )
+}
